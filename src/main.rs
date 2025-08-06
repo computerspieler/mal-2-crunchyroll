e73299f0a528639876cc6321f231d3f6a5085fc7
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2025-08-06 21:33:41 +0200

    Last fixes used to load my history into Crunchyroll

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
-use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
-use dotenvy;
 use anyhow::Result;
-use mal_api::prelude::*;
-use crunchyroll_rs::{Crunchyroll, Locale};
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
 use crunchyroll_rs::common::StreamExt;
+use crunchyroll_rs::{Crunchyroll, Locale};
+use dotenvy;
+use mal_api::prelude::*;
 use reqwest::Response;
 use std::{collections::HashSet, env, thread, time::Duration};
 
 fn get_node_title(node: AnimeFields) -> String {
     match node.alternative_titles {
-    Some(x) => {
-        match x.en {
-        Some(x) if x.len() > 0 => x,
-        _ => node.title
-        }
-    },
-    None => node.title
+        Some(x) => match x.en {
+            Some(x) if x.len() > 0 => x,
+            _ => node.title,
+        },
+        None => node.title,
     }
 }
 
 async fn read_mal_entries() -> Result<Vec<AnimeListNode>> {
-    let mal_username = env::var("MAL_USERNAME")
-        .expect("'MAL_USERNAME' environment variable not found");
+    let mal_username =
+        env::var("MAL_USERNAME").expect("'MAL_USERNAME' environment variable not found");
 
     let client_id = MalClientId::try_from_env()?;
     let api_client = AnimeApiClient::from(&client_id);
-    
+
     let mut output: Vec<AnimeListNode> = vec![];
     let max_page_size = 1000;
     let mut offset = 0;
@@ -42,7 +40,7 @@ async fn read_mal_entries() -> Result<Vec<AnimeListNode>> {
                 AnimeField::list_status,
                 AnimeField::title,
                 AnimeField::alternative_titles,
-                AnimeField::start_date
+                AnimeField::start_date,
             ]))
             .sort(UserAnimeListSort::AnimeStartDate)
             .build()?;
@@ -54,20 +52,18 @@ async fn read_mal_entries() -> Result<Vec<AnimeListNode>> {
             }
             Ok(r) => {
                 done = r.data.len() != (max_page_size as usize);
-                output.extend(r.data.into_iter()
-                    .filter(|elt| {
-                        let status = &elt.list_status;
-                        if status.is_none() {
-                            return false;
-                        }
-                        let status = status.as_ref().unwrap();
-                        if status.num_episodes_watched == 0 {
-                            return false;
-                        }
+                output.extend(r.data.into_iter().filter(|elt| {
+                    let status = &elt.list_status;
+                    if status.is_none() {
+                        return false;
+                    }
+                    let status = status.as_ref().unwrap();
+                    if status.num_episodes_watched == 0 {
+                        return false;
+                    }
 
-                        true
-                    })
-                );
+                    true
+                }));
             }
         }
 
@@ -87,18 +83,21 @@ fn same_title(p: &str, s: &str) -> bool {
         return false;
     }
     /*
-        We need the minimal edit distance here because there is
-        discrepancies between MAL's naming & CR's naming.
-        Ex.:
-            - hitoribocchi no marumaru seikatsu vs. hitoribocchi no marumaruseikatsu
-            - ...
-        And the 0.125 value is just a guess. For a 20 letters title,
-        the maximum distance is 2.
-     */
+       We need the minimal edit distance here because there is
+       discrepancies between MAL's naming & CR's naming.
+       Ex.:
+           - hitoribocchi no marumaru seikatsu vs. hitoribocchi no marumaruseikatsu
+           - ...
+       And the 0.125 value is just a guess. For a 20 letters title,
+       the maximum distance is 2.
+    */
     let score = (levenshtein::levenshtein(p, &s[..n]) as f32) / (n as f32);
-    
+
     if score >= 0.01 {
-        eprintln!("[WARNING] {} => {} ({} {})", s, p,
+        eprintln!(
+            "[WARNING] {} => {} ({} {})",
+            s,
+            p,
             score,
             levenshtein::levenshtein(p, &s[..n])
         );
@@ -112,13 +111,14 @@ struct MarkAsWatch<'a> {
     account_uuid: String,
     current_bearer_token: String,
     preferred_audio: String,
-    locale: String
+    locale: String,
 }
 
 impl<'a> MarkAsWatch<'a> {
-    async fn new(crunchyroll: &'a Crunchyroll,
+    async fn new(
+        crunchyroll: &'a Crunchyroll,
         preferred_audio: Locale,
-        locale: Locale
+        locale: Locale,
     ) -> Result<Self> {
         let account = crunchyroll.account().await?;
         let mut output = Self {
@@ -149,22 +149,17 @@ impl<'a> MarkAsWatch<'a> {
         )
             .bearer_auth(&self.current_bearer_token)
             .build()?;
-    
-        Ok(self.crunchyroll.client()
-            .execute(query)
-            .await?
-        )
+
+        Ok(self.crunchyroll.client().execute(query).await?)
     }
 
     async fn mark(&mut self, content_id: &String) -> Result<()> {
         let res = self._mark_internal(content_id).await?;
-    
+
         if res.status().as_u16() == 401 {
             self.update_token().await?;
 
-            self._mark_internal(content_id)
-                .await?
-                .error_for_status()?;
+            self._mark_internal(content_id).await?.error_for_status()?;
         } else {
             res.error_for_status()?;
         }
@@ -216,47 +211,34 @@ fn parse_date(x: &String) -> NaiveDate {
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
 
-    let email = env::var("EMAIL")
-        .expect("'EMAIL' environment variable not found");
-    let password = env::var("PASSWORD")
-        .expect("'PASSWORD' environment variable not found");
+    let email = env::var("EMAIL").expect("'EMAIL' environment variable not found");
+    let password = env::var("PASSWORD").expect("'PASSWORD' environment variable not found");
 
     let preferred_audio = Locale::from(
-        env::var("PREFERRED_AUDIO")
-            .expect("'PREFERRED_AUDIO' environment variable not found")
+        env::var("PREFERRED_AUDIO").expect("'PREFERRED_AUDIO' environment variable not found"),
     );
-    let locale = Locale::from(
-        env::var("CLOCALE")
-            .expect("'CLOCALE' environment variable not found")
-    );
-    
+    let locale =
+        Locale::from(env::var("CLOCALE").expect("'CLOCALE' environment variable not found"));
+
     let crunchyroll = Crunchyroll::builder()
         .preferred_audio_locale(preferred_audio.clone())
         .login_with_credentials(email, password)
         .await?;
 
-    let mut mark_as_watcher = MarkAsWatch::new(
-        &crunchyroll,
-        preferred_audio,
-        locale
-    ).await?;
+    let mut mark_as_watcher = MarkAsWatch::new(&crunchyroll, preferred_audio, locale).await?;
 
     let mut treated_ids = HashSet::<String>::new();
     let animes = read_mal_entries().await?;
-    let max_date_difference = chrono::TimeDelta::days(2*30);
+    let max_date_difference = chrono::TimeDelta::days(2 * 30);
 
     for elt in animes {
         let (node, status) = (elt.node, elt.list_status);
-        let air_start_date: Option<DateTime<Utc>> = 
-            match node.start_date.as_ref() {
+        let air_start_date: Option<DateTime<Utc>> = match node.start_date.as_ref() {
             None => None,
-            Some(x) => {
-                Utc.from_local_datetime(&NaiveDateTime::new(
-                    parse_date(x),
-                    NaiveTime::default()
-                )).single()
-            }
-            };
+            Some(x) => Utc
+                .from_local_datetime(&NaiveDateTime::new(parse_date(x), NaiveTime::default()))
+                .single(),
+        };
         // We can do it, the status-less entries
         // have been filtered
         let status = status.unwrap();
@@ -270,7 +252,7 @@ async fn main() -> Result<()> {
         if let Some(s) = query_result.series.next().await {
             let series = s?;
             eprintln!("Result '{}' '{}'", &series.title.to_lowercase(), &title);
-    
+
             if same_title(&series.title.to_lowercase(), &title) {
                 let seasons: Vec<crunchyroll_rs::Season> = series.seasons().await?;
                 'SEASON: for season in seasons {
@@ -288,14 +270,14 @@ async fn main() -> Result<()> {
                                     break;
                                 }
 
-                                if episode.episode_air_date >= (date+max_date_difference) {
+                                if episode.episode_air_date >= (date + max_date_difference) {
                                     break 'SEASON;
                                 }
-                            }    
+                            }
                         } else {
                             eprintln!("[WARNING] No date has been found");
                         }
-                        
+
                         if !valid_season {
                             continue;
                         }
@@ -305,8 +287,10 @@ async fn main() -> Result<()> {
                     eprintln!("Found {}", &season.title);
                     if status.num_episodes_watched == season.number_of_episodes {
                         match mark_as_watcher.mark(&season.id).await {
-                        Ok(()) => (),
-                        Err(e) => { dbg!(e); }
+                            Ok(()) => (),
+                            Err(e) => {
+                                dbg!(e);
+                            }
                         }
                     } else {
                         for episode in season.episodes().await? {
@@ -321,16 +305,18 @@ async fn main() -> Result<()> {
                                 }
                             }
                             match mark_as_watcher.mark(&episode.id).await {
-                            Ok(()) => (),
-                            Err(e) => { dbg!(e); }
+                                Ok(()) => (),
+                                Err(e) => {
+                                    dbg!(e);
+                                }
                             }
-                        }    
+                        }
                     }
                     treated_ids.insert(season.title);
                     break;
                 }
             }
-        }    
+        }
 
         if !found {
             println!("{}", title);
